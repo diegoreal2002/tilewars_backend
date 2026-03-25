ef1e13f0dba05bc8b4b33383b0a1d071ff3cf920
diego <diegoreal2002@gmail.com>
2026-03-24 21:32:12 -0500

    first commit

---
+++ src/api/usua/controllers/usua.ts
@@ -0,0 +1,7 @@
+/**
+ * usua controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::usua.usua');
---
+++ src/api/usua/services/usua.ts
@@ -0,0 +1,7 @@
+/**
+ * usua service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::usua.usua');
